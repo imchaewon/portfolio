9c85dd04346de9e4839aa77005ededac295dc9e9
imchaewon <76411348+imchaewon@users.noreply.github.com>
2021-05-23 10:51:20 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -192,7 +192,7 @@ $(window).scroll(function(){
 
 });
 
-$(".sct3 .swiper-wrapper").ready(function(){
+$(".sct3 .swiper-slide").ready(function(){
 	$(".sct3 li a").each(function(){
 		$(this).colorbox();
 	});
