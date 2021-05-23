97673bd95e18ef89b4a0f5fa97e6cf133022e7b3
imchaewon <76411348+imchaewon@users.noreply.github.com>
2021-05-23 22:30:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -103,6 +103,7 @@ $(window).load(function(){
 			// 개별적으로 Wheel 이벤트 적용
 			$(this).children().on("mousewheel DOMMouseScroll", function(e) {
 				if(sw == 1){
+					console.log(123123);
 					console.log(t);
 					return;
 				}
