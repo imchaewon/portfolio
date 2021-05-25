4ad53aed510d1e8a1af1da285978a6998bf75dde
imchaewon <76411348+imchaewon@users.noreply.github.com>
2021-05-26 00:13:11 +0900

    1

--- js/main.js
+++ js/main.js
@@ -105,14 +105,11 @@ $(window).load(function(){
 		$(".section").each(function(){
 			// 개별적으로 Wheel 이벤트 적용
 			$(this).children().on("mousewheel DOMMouseScroll", function(e) {
-				console.log(111);
 				if(sw == 1){
 					t.addClass("hide");
 					return;
 				}
-				console.log(222);
 				e.preventDefault();
-				console.log(333);
 				let delta = 0;
 				if (!event) event = window.event;
 				if (event.wheelDelta) {
@@ -124,8 +121,6 @@ $(window).load(function(){
 					delta = -event.detail / 3;
 				}
 				let moveTop = null;
-				console.log(444);
-				console.log($(this));
 				if (delta < 0) { // 마우스휠을 위에서 아래로
 					if ($(this).next() != undefined && $(this).next().offset() != undefined) {
 						let i = parseInt(this.classList[0].substr(3,1)) + 1;
@@ -150,7 +145,6 @@ $(window).load(function(){
 					scrollTop: moveTop + 'px'
 				}, {
 					duration:800,easing:"easeOutQuad",complete:function () {
-						console.log("이동완료");
 					}
 				});
 			});
